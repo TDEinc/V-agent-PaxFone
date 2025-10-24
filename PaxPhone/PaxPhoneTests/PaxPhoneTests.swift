2fe940c9c9ddf4b54a73821e32dd5323f6b69947
Victor Cruz Cid <victor@tde.rocks>
2025-10-24 17:28:42 -0500

    chore: initialize Xcode project structure

---
+++ PaxPhone/PaxPhoneTests/PaxPhoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaxPhoneTests.swift
+//  PaxPhoneTests
+//
+//  Created by Victor Cruz Cid on 10/24/25.
+//
+
+import Testing
+@testable import PaxPhone
+
+struct PaxPhoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
